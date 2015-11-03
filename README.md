423480f956dad65fa60e0ad12043c259d92e24af
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-03 01:30:36 +0000

    Modified view to display tweets (look and feel to be improved).

---
+++ README.md
@@ -0,0 +1,2 @@
+# TwitterRepManagement
+An application that allows you to view and manage the reputation of an entity over Twitter.
--- twitter_services/views.py
+++ twitter_services/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 from .models import Tweet
+import json
 
 # Create your views here.
 def main(request):
     tweets_list = Tweet.objects.all()
-    template = loader.get_template('twitter_services/main.html')
-    context = {'tweets_list': tweets_list}
+    tweets_json = []
+    tweets_category = []
+    for tweet in tweets_list:
+        tweets_json.append(json.dumps(tweet.tweet_json))
+        tweets_category.append(tweet.reputation_category)
+    context = {'tweets_json': tweets_json,
+               'tweets_category': tweets_category}
     return render(request, 'twitter_services/main.html', context)
