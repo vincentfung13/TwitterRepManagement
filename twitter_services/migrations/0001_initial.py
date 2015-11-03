53f86682176da8c7ce068bb31ffa8d053fa5ce74
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-03 16:49:26 +0000

    Changed data model, added new relation for the one-to-many relationship between tweets and dimensions

--- twitter_services/migrations/0001_initial.py
+++ twitter_services/migrations/0001_initial.py
@@ -13,10 +13,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tweet',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('tweet_id', models.CharField(max_length=100)),
+                ('tweet_id', models.CharField(default=b'Undefined: 2015-11-03 16:45:58.301546+00:00', max_length=50, serialize=False, primary_key=True)),
                 ('tweet_json', models.TextField()),
-                ('repustiaon_category', models.CharField(max_length=20)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tweet_Reputation_Dimension',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('entity', models.CharField(max_length=50)),
+                ('dimension', models.CharField(max_length=20)),
+                ('tweet', models.ForeignKey(to='twitter_services.Tweet')),
             ],
         ),
     ]
--- twitter_services/models.py
+++ twitter_services/models.py
@@ -2,11 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Tweet(models.Model):
-    tweet_id = models.CharField(max_length = 50, primary_key = True, default = 'Undefined: '+ str(timezone.now()))
+    tweet_id = models.CharField(max_length=50, primary_key=True, default='Undefined: ' + str(timezone.now()))
     tweet_json = models.TextField()
-    reputation_category = models.TextField()
-    #sentiment = models.CharField(max_length = 10)
+    # sentiment = models.CharField(max_length = 10)
 
     def __unicode__(self):
         return 'tweet_id: ' + self.tweet_id
+
+
+class Tweet_Reputation_Dimension(models.Model):
+    tweet = models.ForeignKey(Tweet)
+    entity = models.CharField(max_length=50)
+    dimension = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return '{ entity: %s; tweet_id: %s; dimension: %s;}' % (self.entity, self.tweet, self.dimension)
