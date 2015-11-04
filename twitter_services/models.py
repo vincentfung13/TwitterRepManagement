7c7019c0069378597dc8020f42c3b2556b2e7edf
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-04 08:20:20 +0000

    Added table for training set of tweets

--- twitter_services/models.py
+++ twitter_services/models.py
@@ -12,8 +12,17 @@ class Tweet(models.Model):
     def __unicode__(self):
         return 'tweet_id: ' + self.tweet_id
 
+# Table to store training set of tweets
+class Tweet_Training(models.Model):
+    tweet_id = models.CharField(max_length=50, primary_key=True, default='Undefined: ' + str(timezone.now()))
+    tweet_json = models.TextField()
+    # sentiment = models.CharField(max_length = 10)
+
+    def __unicode__(self):
+        return 'tweet_id: ' + self.tweet_id
 
 class Tweet_Reputation_Dimension(models.Model):
+    id = models.TextField(primary_key=True, default='Undefined: ' + str(timezone.now()))
     tweet = models.ForeignKey(Tweet)
     entity = models.CharField(max_length=50)
     dimension = models.CharField(max_length=20)
--- twitter_services/views.py
+++ twitter_services/views.py
@@ -6,10 +6,7 @@ import json
 def main(request):
     tweets_list = Tweet.objects.all()
     tweets_json = []
-    tweets_category = []
     for tweet in tweets_list:
         tweets_json.append(json.dumps(tweet.tweet_json))
-        tweets_category.append(tweet.reputation_category)
-    context = {'tweets_json': tweets_json,
-               'tweets_category': tweets_category}
+    context = {'tweets_json': tweets_json}
     return render(request, 'twitter_services/main.html', context)
