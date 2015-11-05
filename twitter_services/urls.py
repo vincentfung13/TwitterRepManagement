de07cb9bd5d6ac72b3e9cb3a70edc2651f8dfdfa
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-05 23:07:13 +0000

    Added views to display tweets of each entity's

--- twitter_services/urls.py
+++ twitter_services/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-               url(r'^$', views.main, name='main'),
-               ]
+    url(r'main', views.main, name='main'),
+    url(r'^entity/(?P<entity>RL([0-9]|[A-Z])+)/', views.tweets_filter_entity, name='tweets_filtered_entity'),
+]
--- twitter_services/views.py
+++ twitter_services/views.py
@@ -2,33 +2,43 @@ from django.shortcuts import render
 from .models import Tweet, Tweet_Reputation_Dimension
 import json
 
+# TODO: find a clever approach for the block below
+entities_set = Tweet_Reputation_Dimension.objects.all().values('entity').distinct()
+entities_list = []
+for item in entities_set:
+    entities_list.append(item['entity'])
+
+dimension_set = Tweet_Reputation_Dimension.objects.all().values('dimension').distinct()
+dimension_list = []
+for item in dimension_set:
+    dimension_list.append(item['dimension'])
+
 
 # Create your views here.
 def main(request):
-    tweets_list = Tweet.objects.all()
+    tweets_all = Tweet.objects.all()
     tweets_json = []
 
-    entities_set = Tweet_Reputation_Dimension.objects.all().values('entity').distinct()
-    # TODO: find a way to do make the upper line return a list of values only, look at QuerySet API.
-    entities_list = []
-    for item in entities_set:
-        entities_list.append(item['entity'])
-
-    for tweet in tweets_list:
+    for tweet in tweets_all:
         tweets_json.append(json.dumps(tweet.tweet_json))
-    context = {'tweets_json': tweets_json,
-               'entities_list': entities_list}
+    context = {'tweets': tweets_json,
+               'entities_list': entities_list,
+               }
     return render(request, 'twitter_services/main.html', context)
 
 
 def tweets_filter_entity(request, entity):
     tweets_all = Tweet.objects.all()
     tweets_filtered = []
+
     for tweet in tweets_all:
         for entity_dimension_pair in tweet.tweet_reputation_dimension_set.all():
             if entity == entity_dimension_pair.entity:
-                tweets_filtered.append(tweet)
-                break
-    # TODO: create a new template page, figure out how to pass dimension to it
-    context = {'tweets_filtered': tweets_filtered}
-    return render(request, 'twitter_services/filter_entities.html', context)
+                tweets_filtered.append(json.dumps(tweet.tweet_json))
+                continue
+    context = {'tweets': tweets_filtered,
+               'entity': entity,
+               'entities_list': entities_list,
+               'dimension_list': dimension_list
+               }
+    return render(request, 'twitter_services/filter_entity.html', context)
