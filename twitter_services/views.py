92bb0f754435b26207ec2e030deaa3e91f13d787
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-06 00:15:06 +0000

    Added display of each tweet's reputation dimension

--- twitter_services/views.py
+++ twitter_services/views.py
@@ -16,11 +16,14 @@ for item in dimension_set:
 
 # Create your views here.
 def main(request):
-    tweets_all = Tweet.objects.all()
     tweets_json = []
 
-    for tweet in tweets_all:
-        tweets_json.append(json.dumps(tweet.tweet_json))
+    for tweet in Tweet.objects.all():
+        tweet_json = json.loads(tweet.tweet_json)
+        tweet_json['reputation_dimension'] \
+            = ', '.join('%s: %s' % (rep.entity, rep.dimension)
+                                for rep in tweet.tweet_reputation_dimension_set.all())
+        tweets_json.append(json.dumps(tweet_json))
     context = {'tweets': tweets_json,
                'entities_list': entities_list,
                }
@@ -28,13 +31,16 @@ def main(request):
 
 
 def tweets_filter_entity(request, entity):
-    tweets_all = Tweet.objects.all()
     tweets_filtered = []
 
-    for tweet in tweets_all:
+    for tweet in Tweet.objects.all():
         for entity_dimension_pair in tweet.tweet_reputation_dimension_set.all():
             if entity == entity_dimension_pair.entity:
-                tweets_filtered.append(json.dumps(tweet.tweet_json))
+                tweet_json = json.loads(tweet.tweet_json)
+                tweet_json['reputation_dimension'] \
+                    = ', '.join('%s: %s' % (rep.entity, rep.dimension)
+                                for rep in tweet.tweet_reputation_dimension_set.all())
+                tweets_filtered.append(json.dumps(tweet_json))
                 continue
     context = {'tweets': tweets_filtered,
                'entity': entity,
