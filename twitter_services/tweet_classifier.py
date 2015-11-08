43f9585166957619339ad42b1e6e5411d7d558f0
Vincent Fung <vincentfung13@users.noreply.github.com>
2015-11-08 03:00:59 +0000

    Big fix in classifier, improved tokenization and added SVM algorithms

--- twitter_services/tweet_classifier.py
+++ twitter_services/tweet_classifier.py
@@ -4,28 +4,35 @@ import nltk
 import sys
 import string
 from nltk.corpus import stopwords
-from nltk import word_tokenize
+from nltk.tokenize import TweetTokenizer
 from nltk.stem import *
 
+import nltk.classify
+from sklearn.svm import LinearSVC
 
 # Returns the feature set of the given tweet, tweet should be a json string
 def extract_feature(tweet):
     tweet_words = __process_text(tweet)
     features = {}
-    for word in dictionary:
+    for word in word_feature:
         features['contains({})'.format(word)] = (word in tweet_words)
     return features
 
 
-# The argument tweet should be a json string
+# Given a individual tweet, the function returns a list of tokens
 def __process_text(tweet):
-    stopset = list(set(stopwords.words('english')))
-    stemmer = SnowballStemmer("english")
-    tweet_tokens = word_tokenize(json.loads(tweet)['text'].encode('ascii', 'ignore').lower())
+    stop_words = list(set(stopwords.words('english')))
+
+    # Get rid of username handles, replace repeated sequence then tokenize the tweet
+    tweet_tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
+    tweet_tokens = tweet_tokenizer.tokenize(json.loads(tweet)['text'].encode('ascii', 'ignore').lower())
     tweet_words = [word for word in tweet_tokens
-                   if not __contains_punctuation(word) and word not in stopset and word != '\n']
+                   if not __contains_punctuation(word) and word not in stop_words and word != '\n']
+
     # Appliy stemming and remove duplicates words
+    stemmer = SnowballStemmer("english")
     tweet_words = list(set([stemmer.stem(word) for word in tweet_words]))
+
     return tweet_words
 
 
@@ -35,33 +42,43 @@ def __contains_punctuation(word):
     return False
 
 
-# Hard-coded
-def get_dimension(tweet, classification_result):
-    tweet_id = json.loads(tweet)['id_str']
-    for line in classification_result:
-        if line[17:35] == tweet_id and line[38:len(line) - 2] is not None:
-            return line[38:len(line) - 2]
-        else:
-            return 'Undefined'
-
-
 # Construct a dictionary at initialization
-with open(os.getcwd() + 'resources/Tweets/pre.3ent.json', 'r') as json_file:
+with open(os.getcwd() + '/resources/Tweets/pre.3ent.json', 'r') as json_file:
     all_tweets = [line for line in json_file]
 
 dictionary = []
 for tweet in all_tweets:
     dictionary.extend(__process_text(tweet))
-dictionary = list(nltk.FreqDist(dictionary))[200:]
+dictionary = list(nltk.FreqDist(dictionary).most_common(2000))
+word_feature = [entry[0] for entry in dictionary]
 
-with open(sys.path[0] + 'resources/Tweets/pre.3ent.gold', 'r') as classification_results:
-    feature_sets = [(extract_feature(tweet), get_dimension(tweet, classification_results)) for tweet in all_tweets]
+with open(sys.path[0] + '/resources/Tweets/pre.3ent.gold', 'r+') as classification_results:
+    dimension_dict = {}
+    for entry in classification_results:
+        dimension_dict[entry[17:35]] = entry[38:len(line) - 2]
+
+feature_sets = [(extract_feature(tweet), dimension_dict[json.loads(tweet).get('id_str')]) for tweet in all_tweets
+                if dimension_dict[json.loads(tweet).get('id_str')] is not None]
 
 training_set, test_set = feature_sets[1024:], feature_sets[:1024]
 
 print 'Training the classifier'
-classifier = nltk.NaiveBayesClassifier.train(training_set)
+# classifier = nltk.NaiveBayesClassifier.train(training_set)
 # classifier = nltk.classify.DecisionTreeClassifier.train(training_set, entropy_cutoff=0, support_cutoff=0)
+classifier = nltk.classify.SklearnClassifier(LinearSVC())
+classifier.train(training_set)
+
+# with open(sys.path[0] + '/resources/Tweets/pre.3ent.gold', 'r') as classification_results:
+#     for item in all_tweets[1024:]:
+#         dimension = dimension_dict[json.loads(item).get('id_str')]
+#         # if dimension is not None:
+#         print dimension
+#
+#     for item in all_tweets[1025: len(all_tweets) - 1]:
+#         dimension = dimension_dict[json.loads(item).get('id_str')]
+#         classified = classifier.classify(extract_feature(item))
+#         if dimension is not None:
+#             print dimension, classified
 
 print 'Classifying the testing set'
 print 'The accuracy of this experiment is: ' + str(nltk.classify.accuracy(classifier, test_set))
