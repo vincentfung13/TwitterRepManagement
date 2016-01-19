5808f6e359f5b099f051805e8e936a6bccdcfb58
Vincent Fung <vincentfung13@users.noreply.github.com>
2016-01-19 22:17:10 +0000

    Ajax calls to make a login table on tweet display pages

--- TwitterRepManagement/settings.py
+++ TwitterRepManagement/settings.py
@@ -106,8 +106,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Using persistent message storage
-MESSAGE_STORAGE = 'stored_messages.storage.PersistentStorage'
+# MESSAGE_STORAGE = 'stored_messages.storage.PersistentStorage'
