fd5ffddae4a212d621680b63fd2f44a7c27c9fa5
Vincent Fung <vincentfung13@users.noreply.github.com>
2016-01-20 23:21:20 +0000

    New models added for notification framework and links for each entity added in index page

--- user_handle/models.py
+++ user_handle/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from twitter_services.models import Tweet
+
+
+# This model represnets the message that is sent to the user
+class Message(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+    entity = models.CharField(max_length=30)
+    message = models.TextField()
+    tweets = models.ManyToManyField(Tweet)
 
 
 # This model represents the relation between users and the certain entities they are interested in
@@ -9,3 +18,9 @@ class UserEntity(models.Model):
 
     class Meta:
         unique_together = ['user', 'entity']
+
+
+# This model is an abstraction of users' inbox
+class UserMessage(models.Model):
+    user = models.ForeignKey(User)
+    message = models.ManyToManyField(Message)
