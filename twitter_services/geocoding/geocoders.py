cdadd2c76e64164be0d5de02d7a12a192d9b31fb
Vincent Fung <vincentfung13@users.noreply.github.com>
2016-01-31 17:48:49 +0000

    Improved geocoding by adding US states and countries databases

--- twitter_services/geocoding/geocoders.py
+++ twitter_services/geocoding/geocoders.py
@@ -1,9 +1,11 @@
 from TwitterRepManagement import settings
+import csv
 import string
 
 
 class LocalGeocoder(object):
     def __init__(self):
+        # Import world cities database
         with open(settings.BASE_DIR + '/resources/twitter_services/geocoder/cities15000.txt', 'r') as geonames_db:
             self.location_dict = {}
             for line in geonames_db:
@@ -19,6 +21,19 @@ class LocalGeocoder(object):
                 lon = float(line[5])
 
                 self.location_dict.update(dict.fromkeys(alternate, [lat, lon]))
+
+        # Import US states database
+        with open(settings.BASE_DIR + '/resources/twitter_services/geocoder/us_states.csv') as us_states:
+            us_states_file = csv.reader(us_states)
+            for row in us_states_file:
+                self.location_dict[row[0].lower()] = [float(row[2]), float(row[3])]
+
+        # Import countries database
+        with open(settings.BASE_DIR + '/resources/twitter_services/geocoder/countries.csv') as countries_latlon:
+            countries = csv.reader(countries_latlon)
+            for row in countries:
+                self.location_dict[row[2].lower()] = [float(row[3]), float(row[4])]
+
 
     # Take a list of tweets and returns a list of coordinates accordingly
     # Location that doesn't have a match will be discarded
