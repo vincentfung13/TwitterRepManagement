bf132101083b6d544c9fd2608209fa21cdd690b1
Vincent Fung <vincentfung13@users.noreply.github.com>
2016-02-23 23:40:03 +0000

    Handled limite track messages of streaming API

--- twitter_services/streaming_services/TweetProcessWorker.py
+++ twitter_services/streaming_services/TweetProcessWorker.py
@@ -20,18 +20,21 @@ class TweetProcessor(multiprocessing.Process):
     def run(self):
         while True:
             status = self.tweet_queue.get()
-            if not self.spam_detector.is_spam(status) and utility.is_reputation_affecting(status):
-                id_str = status['id_str']
-                status['reputation_dimension'] = self.dimension_classifier.classify(status)
-                status['related_entity'] = utility.fetch_entity(status)
-                status['sentiment_score'] = TweetSentimentEvaluator.rate_sentiment(status)
-                self.tweets_buffer.append(Tweet(tweet_id=id_str, tweet=status, created_at=timezone.now()))
-                print 'Tweet %s added to buffer, buffer size %d' % (id_str, len(self.tweets_buffer))
+            try:
+                if not self.spam_detector.is_spam(status) and utility.is_reputation_affecting(status):
+                    id_str = status['id_str']
+                    status['reputation_dimension'] = self.dimension_classifier.classify(status)
+                    status['related_entity'] = utility.fetch_entity(status)
+                    status['sentiment_score'] = TweetSentimentEvaluator.rate_sentiment(status)
+                    self.tweets_buffer.append(Tweet(tweet_id=id_str, tweet=status, created_at=timezone.now()))
+                    print 'Tweet %s added to buffer, buffer size %d' % (id_str, len(self.tweets_buffer))
 
-                if len(self.tweets_buffer) >= self.buffer_size:
-                    Tweet.objects.bulk_create(self.tweets_buffer)
-                    del self.tweets_buffer[:]
-                    print 'INFO: Bulk created 100 tweets %s' % timezone.now()
+                    if len(self.tweets_buffer) >= self.buffer_size:
+                        Tweet.objects.bulk_create(self.tweets_buffer)
+                        del self.tweets_buffer[:]
+                        print 'INFO: Bulk created 100 tweets %s' % timezone.now()
+            except KeyError:
+                print 'INFO: Error message %s' % status
 
 
 
